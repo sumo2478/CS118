7f13b99f153887f8589bbd45706c9d6d268bc62a
Collin Yen <collinyen7@gmail.com>
2014-01-23 10:37:53 -0800

    Implmented basic listener

--- cs118-project1/http-proxy.cc
+++ cs118-project1/http-proxy.cc
@@ -1,12 +1,89 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
 
 using namespace std;
 
+#define SERVER_PORT "14886"
+#define MAX_CONNECTIONS 20
+
+int create_server_listener (const char* port_num)
+{
+    struct addrinfo hints;
+    struct addrinfo* res;
+
+    memset(&hints, 0, sizeof(hints));  // Make sure struct is empty
+
+    // Set hints structure
+    hints.ai_family = AF_INET;       // Allows IPv4
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE;       // Automatic fill in for IP
+
+    if (getaddrinfo(NULL, port_num, &hints, &res) != 0)
+    {
+        fprintf(stderr, "Get address info error\n");
+        return -1;
+    }
+
+    // Initialize the socket
+    int listen_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    if (listen_socket < 0)
+    {
+        fprintf(stderr, "Unable to create server socket\n");
+        return -1;
+    }
+
+    // Bind the socket
+    int status = bind(listen_socket, res->ai_addr, res->ai_addrlen);
+
+    if (status < 0)
+    {
+        fprintf(stderr, "Failed to bind socket\n");
+        return -1;
+    }
+    //cout < "Listening on address: " << res->ai_addr->s_addr << endl;
+    cout << "began listening...\n";
+
+    // Setup the listener
+    if (listen(listen_socket, MAX_CONNECTIONS))
+    {
+        fprintf(stderr, "Failed to set up listener\n");
+        return -1;
+    }
+
+    // Free the used memory
+    freeaddrinfo(res);
+
+    return listen_socket;
+}
+
 int main (int argc, char *argv[])
 {
-  // command line parsing
-  
-  return 0;
+    // command line parsing
+
+
+    // Open a socket for listening
+    int listen_socket = create_server_listener(SERVER_PORT);
+
+    if (listen_socket < 0)
+        return -1;
+
+    while(1)
+    {
+        continue;
+    }
+
+
+
+    return 0;
 }
