080546b403b48f59803808c44a08ad573bd53697
Collin Yen <collinyen7@gmail.com>
2014-01-27 18:11:38 -0800

    Server can now read in requests from client

--- cs118-project1/http-proxy.cc
+++ cs118-project1/http-proxy.cc
@@ -12,14 +12,58 @@
 #include <pthread.h>
 
 #include "compat.h"
+#include "http-headers.h"
 
 using namespace std;
 
-#define SERVER_PORT "14887" // TODO: Change to 14886
-#define MAX_CONNECTIONS 20
-#define BUFFER_SIZE 1024
+#define SERVER_PORT "14886" // TODO: Change to 14886
+#define MAX_CONNECTIONS 20  // Max number of connections allowed to the server
+#define BUFFER_SIZE 1024    // Buffer size that we read in
 
-// Handling the the user request should be done here
+// Structure that is passed into the thread
+typedef struct 
+{
+    int socket_fd; // File descriptor for the socket
+}thread_data_t;
+
+void* handle_connection(void* p)
+{
+    cout << "Handling connection\n";
+
+    thread_data_t* args = (thread_data_t*) p;
+    string request_data;
+
+    // Read in data until there are two new lines in the buffer
+    while (memmem(request_data.c_str(), request_data.length(), "\r\n\r\n", 4) == NULL)
+    {
+        char buf[BUFFER_SIZE];
+        read(args->socket_fd, buf, sizeof(buf));
+        request_data.append(buf);
+        memset(buf, 0, sizeof(buf));
+    }
+
+    cout << "Read in data: "<< request_data << endl;
+
+    // Obtain the HTTP header from the request_data
+    HttpHeaders header;
+    try
+    {
+        header.ParseHeaders(request_data.c_str(), request_data.length());
+    }catch (ParseException err)
+    {
+        cout << "Header parse exception: " << err.what() << endl;
+        
+        string s = "404 Invalid Request\n";
+        write(args->socket_fd, s.c_str(), s.length());
+    }
+        
+    cout << "Exiting" << endl;
+
+    // Close the socket and exit the thread
+    close(args->socket_fd);
+    pthread_exit(NULL);
+}
+
 int create_server_listener (const char* port_num)
 {
     struct addrinfo hints;
@@ -71,9 +115,6 @@ int create_server_listener (const char* port_num)
 
 int main (int argc, char *argv[])
 {
-    // command line parsing
-
-
     // Open a socket for listening
     int listen_socket = create_server_listener(SERVER_PORT);
 
@@ -92,6 +133,19 @@ int main (int argc, char *argv[])
 
         cout << "Connected to: " << new_connection << endl;
         
+        if (new_connection < 0)
+        {
+            cout << "Connection failed...\n";
+            continue;
+        }else
+        {
+            pthread_t thread;
+            thread_data_t* p = new thread_data_t;
+            p->socket_fd = new_connection;
+
+            pthread_create(&thread, NULL, handle_connection, (void*) p);
+            pthread_detach(thread);
+        }
 
     }
 
